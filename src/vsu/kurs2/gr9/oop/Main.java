e6f784e200cddec4b7b371959e8c9f338feadc7c
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2022-09-24 21:28:24 +0300

    не работает но почему хз

--- src/vsu/kurs2/gr9/oop/Main.java
+++ src/vsu/kurs2/gr9/oop/Main.java
@@ -9,8 +9,37 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import static vsu.kurs2.gr9.oop.TrieTree.TrieNode.insert;
+import static vsu.kurs2.gr9.oop.TrieTree.TrieNode.search;
+
 public class Main {
     public static void main(String[] params) {
+        String keys[] = {"Okay", "a", "hello", "world", "static", "nowhere use static", "want", "sleep"};
+
+        String output[] = {"Есть в поиске", "Нет в поиске"};
+
+        TrieTree.TrieNode root = new TrieTree.TrieNode();
+
+        int i;
+        for(i = 0; i < keys.length; i++){
+            insert(keys[i]);
+        }
+
+        if(search("Okay") == true)
+            System.out.println("Okay --- " + output[1]);
+        else System.out.println("Okay --- " + output[0]);
+
+        if(search("sleep") == true)
+            System.out.println("sleep --- " + output[1]);
+        else System.out.println("sleep --- " + output[0]);
+
+        if(search("hello") == true)
+            System.out.println("hello --- " + output[1]);
+        else System.out.println("hello --- " + output[0]);
+
+        if(search("thaw") == true)
+            System.out.println("thaw --- " + output[1]);
+        else System.out.println("thaw --- " + output[0]);
     }
 }
 
--- src/vsu/kurs2/gr9/oop/TrieTree.java
+++ src/vsu/kurs2/gr9/oop/TrieTree.java
@@ -1,7 +1,62 @@
 package vsu.kurs2.gr9.oop;
 
 public class TrieTree {
-    static final int alphabet = 33;
+
+    static final int alfavit = 33;
+
+    static class TrieNode {
+        TrieNode children[] = new TrieNode[alfavit];
+
+        boolean end;
+
+        TrieNode() {
+            end = false;
+            for (int i = 0; i < alfavit; i++) {
+                children[i] = null;
+            }
+        }
+
+        static TrieNode root;
+
+        static void insert(String key) {
+            int lvl;
+            int index;
+            int leight = key.length();
+
+            TrieNode insrt = root;
+
+            for (lvl = 0; lvl < leight; lvl++) {
+                index = key.charAt(lvl) - 'a';
+
+                if (insrt.children[index] == null) {
+                    insrt = insrt.children[index] = new TrieNode();
+                }
+                insrt = insrt.children[index];
+            }
+
+            insrt.end = true;
+        }
+
+        static boolean search(String key) {
+            int lvl;
+            int index;
+            int leight = key.length();
+
+            TrieNode insrt = root;
+
+            for (lvl = 0; lvl < leight; lvl++) {
+                index = key.charAt(lvl) - 'a';
+
+                if (insrt.children[index] == null) {
+                    return false;
+                }
+                insrt = insrt.children[index];
+            }
+            return (insrt.end);
+        }
 
 
+
+
+    }
 }
