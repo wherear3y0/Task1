161732b6f1914c5048cbf046ad7130bc438745be
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2022-09-29 16:41:55 +0300

    Ну я убрал статики И сделал вызов через TrieTree tree = new TrieTree(); Собственно вызов заработал Но опять минус 18

--- src/vsu/kurs2/gr9/oop/TrieTree.java
+++ src/vsu/kurs2/gr9/oop/TrieTree.java
@@ -2,10 +2,10 @@ package vsu.kurs2.gr9.oop;
 
 public class TrieTree {
 
-     static final int alfavit = 33;
+    static final int alfavit = 33;
 
-     class TrieNode {
-           TrieNode[] children = new TrieNode[alfavit];
+    class TrieNode {
+        TrieNode[] children = new TrieNode[alfavit];
 
         boolean end;
 
@@ -17,42 +17,44 @@ public class TrieTree {
         }
     }
 
-     TrieNode root;
-     void insert(String key) {
-         int lvl;
-         int index;
-         int leight = key.length();
+    TrieNode root;
 
-         root = new TrieNode();
-         TrieNode insrt = root;
+    void insert(String key) {
+        int lvl;
+        int index;
+        int leight = key.length();
 
-         for (lvl = 0; lvl < leight; lvl++) {
-             index = key.charAt(lvl) - 'a';
-                if (insrt.children[index] == null)
-                    insrt = insrt.children[index] = new TrieNode();
+        root = new TrieNode();
+        TrieNode insrt = root;
 
-                insrt = insrt.children[index];
-            }
-            insrt.end = true;
-     }
+        for (lvl = 0; lvl < leight; lvl++) {
+            index = key.charAt(lvl) - 'a';
+            if (insrt.children[index] == null)
+                insrt = insrt.children[index] = new TrieNode();
 
-     boolean search(String key) {
-         int lvl;
-         int index;
-         int leight = key.length();
-
-         TrieNode insrt = root;
-
-         for (lvl = 0; lvl < leight; lvl++) {
-             index = key.charAt(lvl) - 'a';
-                if (insrt.children[index] == null)
-                    return false;
-
-                insrt = insrt.children[index];
-            }
-            return (insrt.end);
-     }
-
-
+            insrt = insrt.children[index];
+        }
+        insrt.end = true;
     }
+
+    boolean search(String key) {
+        int lvl;
+        int index;
+        int leight = key.length();
+
+        root = new TrieNode();
+        TrieNode insrt = root;
+
+        for (lvl = 0; lvl < leight; lvl++) {
+            index = key.charAt(lvl) - 'a';
+            if (insrt.children[index] == null)
+                return false;
+
+            insrt = insrt.children[index];
+        }
+        return (insrt.end);
+    }
+
+
+}
 
